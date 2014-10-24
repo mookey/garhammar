bb6b4a2b15292ce5c6e7fc43b325a0e90c46c7e5
Staffan Garhammar <staffan.garhammar@gmail.com>
2014-10-24 22:03:13 +0200

    removed fucked up tabs

--- public/js/components/tabs.js
+++ public/js/components/tabs.js
@@ -9,10 +9,8 @@ define([templatesName, 'components/base', 'utils/utils'], function(templates, Ba
     f.view            = aView;
     f.addTabsListener = addTabsListener;
     f.initContext     = initContext;
-    f.onEvent         = onEvent;
     f.addTabsListener();
     f.initContext();
-    garhammar.registerListener('resize', 'tabs', aView.getAttribute("data-id"));
     return f;
   };
 
@@ -20,18 +18,6 @@ define([templatesName, 'components/base', 'utils/utils'], function(templates, Ba
     this.addTabsListener();
   }
 
-  function onEvent(eventName) {
-    var contentId;
-    var style;
-    if (this.view.querySelector('.js-tabs-link.js-active')) {
-      return;
-    }
-    style = window.getComputedStyle(this.view.querySelector('.js-tabs-large'));
-    if (style.getPropertyValue('display') === 'none') {
-      return;
-    }
-    this.view.querySelector('.js-tabs-link').click();
-  }
 
   function initContext() {
     var activeLink      = this.view.querySelector('a.js-active');
@@ -40,8 +26,7 @@ define([templatesName, 'components/base', 'utils/utils'], function(templates, Ba
       return;
     }
     var contentId       = activeLink.getAttribute('data-content-id');
-    var contents        = this.view.querySelectorAll('.js-tabs-content');
-    var content         = contents[contentId];
+    var content        = this.view.querySelector('.js-tab-content[data-content-id="' + contentId + '"]');
     var sourceUrl       = activeLink.getAttribute('data-runtime-source');
     if (!sourceUrl) {
       content.innerHTML   = templates[runtimeTemplate]();
@@ -61,12 +46,12 @@ define([templatesName, 'components/base', 'utils/utils'], function(templates, Ba
 
   function addTabsListener() {
     var self      = this;
-    var tabLinks  = this.view.querySelectorAll('.js-tabs-link');
+    var tabLinks  = this.view.querySelectorAll('.js-tab');
+
     utils.each(tabLinks, function(link) {
       link.addEventListener('click', function(ev) {
 
         var isActive  = this.classList.contains('js-active');
-        var isSmall   = this.parentNode.classList.contains('js-tabs-wrapper');
         var contentId;
         var previouslyActiveLinks;
         var previouslyActiveContentId;
@@ -74,36 +59,29 @@ define([templatesName, 'components/base', 'utils/utils'], function(templates, Ba
 
         ev.preventDefault();
 
-        if (isActive && !isSmall) {
+        if (isActive) {
           return;
         }
 
-        previouslyActiveLinks     = self.view.querySelectorAll('.js-active');
-        if (previouslyActiveLinks.length) {
-          previouslyActiveContentId = previouslyActiveLinks[0].getAttribute('data-content-id');
-          previouslyActiveContent   = self.view.querySelector('.js-tabs-content[data-content-id="' + previouslyActiveContentId + '"]');
-
+        utils.each(tabLinks, function(tab) {
+          previouslyActiveContentId = tab.getAttribute('data-content-id');
+          previouslyActiveContent   = self.view.querySelector('.js-tab-content[data-content-id="' + previouslyActiveContentId + '"]');
           utils.each(previouslyActiveContent.querySelectorAll('.js-component'), function(c, i) {
             garhammar.removeAllListenersForComponent(c.getAttribute('data-component'));
           });
-          previouslyActiveContent.classList.add('hide');
-
-          utils.each(previouslyActiveLinks, function(link) {
-            link.classList.remove('active');
-            link.classList.remove('js-active');
-          });
-
-          if (isActive && isSmall) {
-            return;
-          }
-        }
-
-        contentId = this.getAttribute('data-content-id');
-        utils.each(self.view.querySelectorAll('.js-tabs-link[data-content-id="' + contentId + '"]'), function(link) {
-          link.classList.add('active');
-          link.classList.add('js-active');
+          tab.classList.remove('active');
+          tab.classList.remove('js-active');
         });
-        self.view.querySelector('.js-tabs-content[data-content-id="' + contentId + '"]').classList.remove('hide');
+        
+        utils.each(self.view.querySelectorAll('.js-tab-content'), function(content) {
+          content.classList.add('hide');
+        });
+
+        this.classList.add('active');
+        this.classList.add('js-active');
+        contentId = this.getAttribute('data-content-id');
+        self.view.querySelector('.js-tab-content[data-content-id="' + contentId + '"]').classList.remove('hide');
+        
         self.initContext();
       });
     });
